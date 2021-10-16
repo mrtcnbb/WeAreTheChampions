f235be48e5f949e2785ee128284b882dc5440ed4
Muratcan Baba <muratcanbaba1@gmail.com>
2021-10-16 14:53:11 +0300

    Main Form is editted.

--- WeAreTheChampions/Form1.Designer.cs
+++ WeAreTheChampions/Form1.Designer.cs
@@ -29,13 +29,130 @@ namespace WeAreTheChampions
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tsmi = new System.Windows.Forms.MenuStrip();
+            this.tsmiTeams = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiColors = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiPlayers = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgvMatches = new System.Windows.Forms.DataGridView();
+            this.btnNewMatch = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.tsmi.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatches)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tsmi
+            // 
+            this.tsmi.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.tsmi.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiTeams,
+            this.tsmiColors,
+            this.tsmiPlayers});
+            this.tsmi.Location = new System.Drawing.Point(0, 0);
+            this.tsmi.Name = "tsmi";
+            this.tsmi.Padding = new System.Windows.Forms.Padding(13, 3, 0, 3);
+            this.tsmi.Size = new System.Drawing.Size(1209, 30);
+            this.tsmi.TabIndex = 0;
+            this.tsmi.Text = "menuStrip1";
+            // 
+            // tsmiTeams
+            // 
+            this.tsmiTeams.Name = "tsmiTeams";
+            this.tsmiTeams.Size = new System.Drawing.Size(65, 24);
+            this.tsmiTeams.Text = "Teams";
+            // 
+            // tsmiColors
+            // 
+            this.tsmiColors.Name = "tsmiColors";
+            this.tsmiColors.Size = new System.Drawing.Size(65, 24);
+            this.tsmiColors.Text = "Colors";
+            // 
+            // tsmiPlayers
+            // 
+            this.tsmiPlayers.Name = "tsmiPlayers";
+            this.tsmiPlayers.Size = new System.Drawing.Size(69, 24);
+            this.tsmiPlayers.Text = "Players";
+            // 
+            // dgvMatches
+            // 
+            this.dgvMatches.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvMatches.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMatches.Location = new System.Drawing.Point(20, 147);
+            this.dgvMatches.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvMatches.Name = "dgvMatches";
+            this.dgvMatches.RowHeadersWidth = 51;
+            this.dgvMatches.RowTemplate.Height = 24;
+            this.dgvMatches.Size = new System.Drawing.Size(1174, 398);
+            this.dgvMatches.TabIndex = 1;
+            // 
+            // btnNewMatch
+            // 
+            this.btnNewMatch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNewMatch.Location = new System.Drawing.Point(989, 62);
+            this.btnNewMatch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnNewMatch.Name = "btnNewMatch";
+            this.btnNewMatch.Size = new System.Drawing.Size(205, 75);
+            this.btnNewMatch.TabIndex = 2;
+            this.btnNewMatch.Text = "New Match";
+            this.btnNewMatch.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnDelete.Location = new System.Drawing.Point(20, 555);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(154, 55);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnEdit.Location = new System.Drawing.Point(198, 555);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(156, 55);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1209, 637);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnNewMatch);
+            this.Controls.Add(this.dgvMatches);
+            this.Controls.Add(this.tsmi);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MainMenuStrip = this.tsmi;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "WeAreTheChampions 1.0";
+            this.tsmi.ResumeLayout(false);
+            this.tsmi.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatches)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip tsmi;
+        private System.Windows.Forms.ToolStripMenuItem tsmiTeams;
+        private System.Windows.Forms.ToolStripMenuItem tsmiColors;
+        private System.Windows.Forms.ToolStripMenuItem tsmiPlayers;
+        private System.Windows.Forms.DataGridView dgvMatches;
+        private System.Windows.Forms.Button btnNewMatch;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
     }
 }
 
